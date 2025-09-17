cb23f4b29cf5fc36e0763ae82cd1430dbf1d51bc
Nav1 <snithfferx@outlook.com>
2025-09-17 02:21:56 -0600

    SIMA | Updating | Types | Response | Refactor Response class for improved readability and consistency; streamline constructor and method formatting

--- src/core/types/Response.php
+++ src/core/types/Response.php
@@ -12,98 +12,67 @@
  * @copyright (c) 2021-2025 Bytes4Run
  */
 
- declare(strict_types=1);
+declare (strict_types = 1);
 
- namespace SIMA\TYPES;
+namespace SIMA\TYPES;
 
- class Response {
-	/**
-	 * @var string | int
-	 * Define the status of the response
-	 */
-	 private $status;
-	 /**
-	  * @var string
-	  * Define the message of the response
-	  */
-	 private $message;
-	 /**
-	  * @var array
-	  * Define the data of the response
-	  */
-	 private $data;
+class Response
+{
+    /**
+     * @var string | int
+     * Define the status of the response
+     */
+    private $status;
+    /**
+     * @var string
+     * Define the message of the response
+     */
+    private $message;
+    /**
+     * @var array
+     * Define the data of the response
+     */
+    private $data;
 
-	 function __construct(string|int $status=200, string $message='', array|null $data=[]) {
-		 $this->status = $status;
-		 $this->message = $message;
-		 $this->data = $data;
-	 }
-	 /* 
-	 Array
-(
-    [code] => 200
-    [message] => ok
-    [data] => Array
-        (
-            [view] => Array
-                (
-                    [type] => view
-                    [name] => home/index
-                )
+    function __construct(string | int $status = 200, string $message = '', array | null $data = [])
+    {
+        $this->status  = $status;
+        $this->message = $message;
+        $this->data    = $data;
+    }
 
-            [data] => Array
-                (
-                    [breadcrumbs] => Array
-                        (
-                            [main] => home
-                            [routes] => Array
-                                (
-                                    [0] => Array
-                                        (
-                                            [text] => home
-                                            [param] => 
-                                            [method] => index
-                                            [controller] => index
-                                        )
+    function getMessage()
+    {
+        return $this->message;
+    }
+    function getStatus()
+    {
+        return $this->status;
+    }
+    function getData()
+    {
+        return $this->data;
+    }
+    function toArray()
+    {
+        return get_object_vars($this);
+    }
 
-                                )
+    function json()
+    {
+        header('Content-Type: application/json'); //Especificamos el tipo de contenido a devolver
+        http_response_code($this->getStatus());
 
-                        )
+        return json_encode($this->getData(), JSON_THROW_ON_ERROR); //Devolvemos el contenido
+    }
 
-                    [datos] => Array
-                        (
-                        )
+    function get()
+    {
+        return $this;
+    }
 
-                )
-
-        )
-
-) */
-
-	 function getMessage() {
-		 return $this->message;
-	 }
-	 function getStatus() {
-		 return $this->status;
-	 }
-	 function getData() {
-		 return $this->data;
-	 }
-	 function toArray() {
-		 return get_object_vars($this);
-	 }
-
-	 function json() {
-		 return json_encode($this);
-	 }
-
-	 function get() {
-		 return $this;
-	 }
-
-	 function asError() {
-		 return new Error($this->status,$this->message);
-	 }
- }
-
-?>
+    function asError()
+    {
+        return new Error($this->status, $this->message);
+    }
+}
